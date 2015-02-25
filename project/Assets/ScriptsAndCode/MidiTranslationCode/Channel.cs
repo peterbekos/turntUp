35fa192a89abe8d6da587c65c0f07bc034809cb4
lstemp2 <lstemp2@uic.edu>
2015-02-25 12:36:50 -0800

    Reorganized some of the files and added midi translation code

---
+++ project/Assets/ScriptsAndCode/MidiTranslationCode/Channel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+    public class Channel
+    {
+        public String ChannelName;
+        public int ChannelNumber;
+        public List<Note> Notes = new List<Note>();
+        public List<String> TypesOfNotes = new List<String>();
+        public Dictionary<string, GameObject> BeatCubeMapping = new Dictionary<string, GameObject>();
+        public int lastNote = 0;
+    }
+
