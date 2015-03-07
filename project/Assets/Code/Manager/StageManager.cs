12e8d5e2723378ddce00bc18a48b69435253c72c
Pete Bekos <peterbekos@gmail.com>
2015-03-07 01:13:10 -0600

    Move manager classes and the game dictionary to the "Manager" folder under "Code"

---
+++ project/Assets/Code/Manager/StageManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+ * This class is used to manage game screens and stages
+ */
+public static class StageManager {
+
+	public static void ping() {}
+
+	static StageManager() {
+		init ();
+	}
+	
+	//variables
+	
+	
+	//initalization function
+	private static void init() {
+		
+	}
+
+	public static void configureEnemies() {
+		//TODO - this method will be called at the begining of the stage
+		// What it will do is determine what enemies will spawn when
+		// from the BeatManager
+	}
+
+	public static void spawnEnemies(float gameTime) {
+		//TODO - spawn enemies based off enemy configureation
+		EnemyObject enemy = new EnemyObject ();
+		//TODO - regiester enemies in GameManager
+		GameManager.enemies.Add (enemy);
+	}
+}
