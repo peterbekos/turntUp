cb54848522f243b250787e63ab3190df524a5126
TankMissile <trevans1@yahoo.com>
2015-04-15 12:02:01 -0500

    uploading missing git files for high scores
    whoops

---
+++ project/Assets/Code/Manager/HighScoreManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class HighScoreManager : MonoBehaviour {
+
+	public HighScoreTable hscores = new HighScoreTable();
+	
+	void Awake(){
+		GameManager.scoretable = this;
+		enableAll(false);
+	}
+	
+	// Use this for initialization
+	public void displayScores () {
+		enableAll(true);
+		hscores.loadScores(GameManager.gameTimer.levelName);
+		GameObject.Find("HighScoreDisplay").GetComponent<Text>().text = "" + hscores.getScore(1);
+		for(int i = 1; i <= 10; i++){
+			GameObject.Find("Score" + i).GetComponent<Text>().text = "" + hscores.getScore(i);
+			GameObject.Find("Name" + i).GetComponent<Text>().text = hscores.getName(i);
+		}
+	}
+	
+	public void enableAll(bool enable){
+		gameObject.SetActive(enable);
+		
+		foreach(Transform t in gameObject.GetComponentInChildren<Transform>()){
+			t.gameObject.SetActive(enable);
+		}
+		
+		/*for(int i = 1; i <= 10; i++){
+			GameObject.Find ("" + i).SetActive(true);
+			GameObject.Find("Score" + i).SetActive(true);
+			GameObject.Find("Name" + i).SetActive(true);
+		}*/
+	}
+	
+	public HighScoreTable getScoreTable(){
+		return hscores;
+	}
+}
