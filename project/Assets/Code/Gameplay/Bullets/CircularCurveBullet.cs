4c6ba9982f5f073ddf9ceba00f6c661daac1a737
lstemp2@uic.edu <lstemp2@uic.edu>
2015-04-25 19:30:09 -0800

    Fixed some mainmenu shtuff

--- project/Assets/Code/Gameplay/Bullets/CircularCurveBullet.cs
+++ project/Assets/Code/Gameplay/Bullets/CircularCurveBullet.cs
@@ -6,10 +6,10 @@ public class CircularCurveBullet : ShotObject {
 	public float rotationSpeed = 180f; //how many degrees to turn in one second
 	
 	// Rotate and move forward (moves in a circle)
-	/*new void Update () {
+	new void Update () {
 		base.Update();
 	
 		transform.Rotate(new Vector3(0, 0, rotationSpeed * Time.deltaTime));
 		rigidbody2D.velocity = transform.up * speed;
-	}*/
+	}
 }
