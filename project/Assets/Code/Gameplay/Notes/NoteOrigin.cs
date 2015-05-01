a3b294b7f8ec73a790df3ae9806c03797b6a09a9
TankMissile <trevans1@yahoo.com>
2015-05-01 05:13:28 -0500

    Aligned notes correctly

--- project/Assets/Code/Gameplay/Notes/NoteOrigin.cs
+++ project/Assets/Code/Gameplay/Notes/NoteOrigin.cs
@@ -24,7 +24,7 @@ public class NoteOrigin : BeatObject
     new public void onMelody(float interp)
     {
         if (greenNote == null) return;
-        spanwnote(greenNote, greenNoteSpawnPoint, interp);
+		spanwnote(greenNote, transform.position + greenNoteSpawnPoint, interp);
     }
 
     // harmony + kick = 2
@@ -32,35 +32,35 @@ public class NoteOrigin : BeatObject
     new public void onHarmony(float interp)
     {
         if (yellowNote == null) return;
-        spanwnote(yellowNote, yellowNoteSpawnPoint, interp);
+        spanwnote(yellowNote, transform.position + yellowNoteSpawnPoint, interp);
     }
     new public void onKick(float interp)
     {
         if (redNote == null) return;
-        spanwnote(redNote, redNoteSpawnPoint, interp);
+		spanwnote(redNote, transform.position + redNoteSpawnPoint, interp);
     }
 
     // kick/snare = 3 since harmony is not really there
     new public void onSnare(float interp)
     {
         if (blueNote == null) return;
-        spanwnote(blueNote, redNoteSpawnPoint, interp);
+		spanwnote(blueNote, transform.position + redNoteSpawnPoint, interp);
     }
     new public void onHat(float interp)
     {
         if (yellowNote == null) return;
-        spanwnote(yellowNote, redNoteSpawnPoint, interp);
+		spanwnote(yellowNote, transform.position + redNoteSpawnPoint, interp);
     }
 
     // treble + bass = 4
     new public void onBass(float interp)
     {
         if (blueNote == null) return;
-        spanwnote(blueNote, blueNoteSpawnPoint, interp);
+		spanwnote(blueNote, transform.position + blueNoteSpawnPoint, interp);
     }
     new public void onTreble(float interp)
     {
         if (blueNote == null) return;
-        spanwnote(blueNote, blueNoteSpawnPoint, interp);
+		spanwnote(blueNote, transform.position + greenNoteSpawnPoint, interp);
     }
 }
